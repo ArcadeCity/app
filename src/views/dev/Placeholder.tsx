0f90bc7c62e0ef2d772ff561da3657746bc68cd8
Christopher David <chris@arcade.city>
2022-08-16 12:01:51 -0500

    Add Placeholder screen

---
+++ src/views/dev/Placeholder.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native'
+import { palette } from 'views/theme'
+
+export const Placeholder = () => {
+  return <View style={{ flex: 1, backgroundColor: palette.radicalRed }} />
+}
