40ba04a54cb858b37dfc79dd6c2b82fc8cb1e9a7
Christopher David <chris@arcade.city>
2022-08-16 12:02:29 -0500

    Placeholder is purple

--- src/views/dev/Placeholder.tsx
+++ src/views/dev/Placeholder.tsx
@@ -2,5 +2,5 @@ import { View } from 'react-native'
 import { palette } from 'views/theme'
 
 export const Placeholder = () => {
-  return <View style={{ flex: 1, backgroundColor: palette.radicalRed }} />
+  return <View style={{ flex: 1, backgroundColor: palette.purple }} />
 }
