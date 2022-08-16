334d3b009b3fca64aa723809ba93f35e8d898d9a
Christopher David <chris@arcade.city>
2022-08-16 11:46:51 -0500

    Update import paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,32 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            i18n: './src/i18n',
+            lib: './src/lib',
+            navigation: './src/navigation',
+            services: './src/services',
+            stores: './src/stores',
+            views: './src/views',
+          },
+        },
+      ],
+    ],
   }
 }
