8b901fb1eb951c912da49f3859e3a9aba80e8a12
Christopher David <chris@arcade.city>
2022-08-16 12:30:16 -0500

    Add Skia and logo

--- src/views/entry/HomeScreen.tsx
+++ src/views/entry/HomeScreen.tsx
@@ -1,5 +1,32 @@
+import { StyleSheet, View } from 'react-native'
 import { FadeInMap } from 'views/map'
+import { Logo } from './Logo'
 
 export const HomeScreen = () => {
-  return <FadeInMap />
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Logo />
+      </View>
+      <FadeInMap />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  containerLogo: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 9000,
+  },
+})
