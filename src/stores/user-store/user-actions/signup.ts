1198dda53972ce06509011daed1ab76ca41d42ae
Christopher David <chris@arcade.city>
2022-08-18 15:41:21 -0500

    randomize placekitten pix

--- src/stores/user-store/user-actions/signup.ts
+++ src/stores/user-store/user-actions/signup.ts
@@ -8,10 +8,14 @@ export interface SignupProps {
 }
 
 export const signup = async (self: UserStore, { username, displayName, about }: SignupProps) => {
+  const width = Math.floor(Math.random() * (220 - 200 + 1)) + 200
+  const height = Math.floor(Math.random() * (320 - 300 + 1)) + 300
+
   const metadata = {
     name: username,
     displayName,
     about,
+    picture: `https://placekitten.com/${width}/${height}`,
     website: null,
   }
 
