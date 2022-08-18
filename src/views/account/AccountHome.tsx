ff64b45127aec7ee0869a56374db32a9cb053d4f
Christopher David <chris@arcade.city>
2022-08-18 14:08:16 -0500

    Show profile info on AccountHome

--- src/views/account/AccountHome.tsx
+++ src/views/account/AccountHome.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Button, StyleSheet, View } from 'react-native'
 import { useStores } from 'stores/root-store'
+import { Text } from 'views/shared'
 import { color } from 'views/theme'
 
 export const AccountHome = () => {
   const { user } = useStores()
   return (
     <View style={styles.container}>
+      <Text text={user.username} preset='header' />
+      <Text text={user.displayName} preset='header' />
+      <Text text={user.about} preset='header' />
       <Button title='Log out' onPress={() => user.logout()} />
     </View>
   )
