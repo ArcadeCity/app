0d88a27c6284028b8c1a75ec3f5a273b23b235f3
Christopher David <chris@arcade.city>
2022-08-29 12:38:44 -0500

    getPublicKey still need to fix

--- src/lib/nostr/keys.ts
+++ src/lib/nostr/keys.ts
@@ -1,6 +1,6 @@
-import schnorr from 'bip-schnorr'
 import { Buffer } from 'buffer'
+import * as secp256k1 from 'tiny-secp256k1-v1/js.js'
 
 export function getPublicKey(privateKey: Buffer) {
-  return Buffer.from(schnorr.getPublicKey(privateKey)).toString('hex')
+  return Buffer.from(secp256k1.schnorr.getPublicKey(privateKey)).toString('hex')
 }
