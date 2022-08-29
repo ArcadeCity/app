262b120f63d01a8bd7aabd9cac76bdd02301af43
Christopher David <chris@arcade.city>
2022-08-29 10:53:56 -0500

    Swap noble for bip-schnorr in keys

--- src/lib/nostr/keys.ts
+++ src/lib/nostr/keys.ts
@@ -1,10 +1,6 @@
+import schnorr from 'bip-schnorr'
 import { Buffer } from 'buffer'
-import * as secp256k1 from '@alephium/noble-secp256k1'
-
-export function generatePrivateKey() {
-  return Buffer.from(secp256k1.utils.randomPrivateKey()).toString('hex')
-}
 
 export function getPublicKey(privateKey: Buffer) {
-  return Buffer.from(secp256k1.schnorr.getPublicKey(privateKey)).toString('hex')
+  return Buffer.from(schnorr.getPublicKey(privateKey)).toString('hex')
 }
