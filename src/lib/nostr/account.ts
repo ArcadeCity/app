78bd76365ea3d7fefd8588d5fc465cb720382cb3
Christopher David <chris@arcade.city>
2022-08-29 12:33:02 -0500

    pubkey chop and console

--- src/lib/nostr/account.ts
+++ src/lib/nostr/account.ts
@@ -1,22 +1,36 @@
 import { bech32 } from 'bech32'
 import { Buffer } from 'buffer'
 import { getPublicKey } from './keys'
-import { generateSeedWords, keypairFromSeed, seedFromWords } from './nip06'
+import { generateSeedWords, keypairFromSeed, privateKeyFromSeed, seedFromWords } from './nip06'
 
 export const createNewAccount = () => {
   const mnemonic = generateSeedWords()
   const seed = seedFromWords(mnemonic)
   const { privateKey, publicKey } = keypairFromSeed(seed)
+  // const publicKey = getPublicKey(Buffer.from(privateKey, 'hex'))
+  const pubbuffer = Buffer.from(publicKey, 'hex')
+  console.log(mnemonic)
+
+  // remove first byte from pubbuffer
+  const pubbuffer2 = pubbuffer.slice(1)
+
+  console.log('PUBBUFFER LENGTH:', pubbuffer.length)
+  console.log('pubbuffer2 LENGTH:', pubbuffer2.length)
+
+  console.log('33byte pubkey:', publicKey)
+  console.log('32byte pubkey:', pubbuffer2.toString('hex'))
+
+  // console.log(pubbuffer)
   return {
     mnemonic,
     privateKey,
-    publicKey,
+    publicKey: pubbuffer2.toString('hex'),
   }
 }
 
 export const getKeysForMnemonic = (mnemonic: string) => {
   const seed = seedFromWords(mnemonic)
-  const privateKey = keypairFromSeed(seed).privateKey as string
+  const privateKey = privateKeyFromSeed(seed) as string
   const publicKey = getPublicKey(Buffer.from(privateKey, 'hex'))
   return {
     privateKey,
