f0f0b69af328d570ecb674ca9d8f54200d98ae93
Christopher David <chris@arcade.city>
2022-09-01 20:59:04 -0500

    Replace app with demo map

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,24 @@
-import 'text-encoding-polyfill'
-import { StatusBar } from 'expo-status-bar'
-import { useCachedResources, useExpoUpdates } from 'lib/hooks'
-import { useEffect, useMemo, useState } from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { RootStore, RootStoreProvider, setupRootStore } from 'stores/root-store'
-import { LoadSplash } from 'views/loading'
-import { Navigation } from './navigation'
+import * as React from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import MapView from 'react-native-maps'
 
-export const App = () => {
-  const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined)
-  const [ready, setReady] = useState(false)
-  const isLoadingComplete = useCachedResources()
-  useExpoUpdates(3)
-
-  useEffect(() => {
-    ;(async () => {
-      try {
-        // await initFonts()
-        const initStore = await setupRootStore(null)
-        setRootStore(initStore)
-        // await delay(500)
-        setReady(true)
-      } catch (e: any) {
-        // notify(e)
-        console.log(e)
-      }
-    })()
-  }, [])
-
-  const componentToRender = useMemo(() => {
-    return !isLoadingComplete || !ready || !rootStore ? (
-      <LoadSplash ready={false} />
-    ) : (
-      <RootStoreProvider value={rootStore}>
-        <SafeAreaProvider>
-          <Navigation />
-        </SafeAreaProvider>
-      </RootStoreProvider>
-    )
-  }, [isLoadingComplete, ready, rootStore])
-
+export function App() {
   return (
-    <>
-      <StatusBar style='light' />
-      <LoadSplash ready={isLoadingComplete} />
-      {componentToRender}
-    </>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+})
