a11eb1c1b7931587ef8a61b8ff9489356b71fc82
Christopher David <chris@arcade.city>
2022-09-03 09:43:46 -0500

    Pull in map styles and dummy data

---
+++ src/views/map/dummyData.ts
@@ -0,0 +1,9 @@
+export const MONARCH_COORDS = {
+  latitude: 30.269265,
+  longitude: -97.751559,
+}
+
+export const PLEBLAB_COORDS = {
+  latitude: 30.26634991002017,
+  longitude: -97.73647131476501,
+}
