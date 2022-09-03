f9bf1d7766f370a16651f22c021b75b0d36d7e9b
Christopher David <chris@arcade.city>
2022-09-03 17:50:12 -0500

    Exerpiment w blue behind logo

--- src/views/map/markers/ImageMarker/ImageMarker.tsx
+++ src/views/map/markers/ImageMarker/ImageMarker.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { Canvas, Image, useImage } from '@shopify/react-native-skia'
+import { Canvas, Circle, Group, Image, LinearGradient, useImage, vec } from '@shopify/react-native-skia'
 
 export const ImageMarker = ({ image }) => {
   const usedImage = useImage(image)
   const height = 80
   const width = 80
+  const r = width / 2
   if (!usedImage) {
     return null
   }
   return (
     <Canvas style={{ width, height }}>
-      <Image image={usedImage} x={0} y={0} width={width} height={height} fit='contain'></Image>
+      <Circle cx={r} cy={r} r={r}>
+        <LinearGradient start={vec(0, 0)} end={vec(3 * r, 3 * r)} colors={['#0061ff', '#60efff']} />
+      </Circle>
+      <Image
+        image={usedImage}
+        x={width * 0.125}
+        y={width * 0.125}
+        width={width * 0.75}
+        height={height * 0.75}
+        fit='contain'></Image>
     </Canvas>
   )
 }
