16cdd80ed41c4b5271b1262707052d5cc70c1dfd
Christopher David <chris@arcade.city>
2022-09-04 03:09:36 -0500

    Add MapHome w DemoMap

---
+++ src/views/map/screens/map-home/map-home.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Platform, StyleSheet, View } from 'react-native'
+import { DemoMap } from 'views/map/demo-map'
+import { PLEBLAB_COORDS } from 'views/map/dummyData'
+import { color } from 'views/theme'
+import MapboxGL from '@rnmapbox/maps'
+
+MapboxGL.setWellKnownTileServer(Platform.OS === 'android' ? 'Mapbox' : 'mapbox')
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiYWNsaW9ucyIsImEiOiJjamVhMmNtY2swaXNtMnBsbnB2aDVqNTBiIn0.gM_i1jhawFz2EpKBX4VmwQ'
+)
+
+export const MapHome = () => {
+  return (
+    <View style={styles.page}>
+      <DemoMap
+        centerCoordinate={[PLEBLAB_COORDS.longitude, PLEBLAB_COORDS.latitude]}
+        zoomLevel={15}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  page: {
+    alignItems: 'center',
+    backgroundColor: color.background,
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
+})
