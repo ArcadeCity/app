1458a35378daf0feafe0f3d11e69e1cbf8b66fad
Christopher David <chris@arcade.city>
2022-09-05 17:18:17 -0500

    Config metro for r3f

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,7 @@ const projectRoot = __dirname
 const config = getDefaultConfig(projectRoot)
 
 config.resolver.extraNodeModules = extraNodeModules
+config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs']
+config.resolver.assetExts = ['glb', 'gltf', 'png', 'jpg']
 
 module.exports = config
