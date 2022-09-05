5fd752488886f93e262e83524897a20b7c4589e8
Christopher David <chris@arcade.city>
2022-09-05 17:39:32 -0500

    metroconfig push not set

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,6 @@ const projectRoot = __dirname
 const config = getDefaultConfig(projectRoot)
 
 config.resolver.extraNodeModules = extraNodeModules
-config.resolver.sourceExts = ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs']
-config.resolver.assetExts = ['glb', 'gltf', 'png', 'jpg']
+config.resolver.assetExts.push('glb', 'gltf')
 
 module.exports = config
