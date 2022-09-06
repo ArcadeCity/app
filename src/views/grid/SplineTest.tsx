1af9404a6fe1fbe766531a80b139ee6776c98eb8
Christopher David <chris@arcade.city>
2022-09-05 19:41:43 -0500

    spline test works

--- src/views/grid/SplineTest.tsx
+++ src/views/grid/SplineTest.tsx
@@ -2,11 +2,12 @@ import { Clone, Float as FloatImpl } from '@react-three/drei/native'
 import useSpline from '@splinetool/r3f-spline'
 
 export function SplineTest({ ...props }) {
-  const { nodes } = useSpline('https://staging.arcade.city/untitled.spline')
+  const { nodes } = useSpline('https://staging.arcade.city/balls2.spline')
 
   return (
     <group {...props} dispose={null} position={[0, -2, -15]} scale={0.01}>
       <Float object={nodes['Sphere']} />
+      <Float object={nodes['Sphere 2']} />
     </group>
   )
 }
